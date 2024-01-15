8bd8b9971335a5cc2e6b11d68b0806f0cb564621
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-01-15 12:03:43 -0500

    Adding ferch functionalities to the model

--- models/post.js
+++ models/post.js
@@ -11,6 +11,19 @@ class Post {
             }
     }
 
+    static async fetchAll() {
+        const posts = await db.getDb().collection('posts').find().toArray();
+        return posts;
+    }
+
+    async fetch() {
+        if (!this.id) {
+            return;
+        }
+        const postDocument = await db.getDb().collection('posts').findOne({ _id: this.id });
+        this.title = postDocument.title;
+        this.content = postDocument.content;
+    }
     async save() {
         let result;
         if (this.id) {
@@ -35,6 +48,7 @@ class Post {
             return;
         }
         const result = await db.getDb().collection('posts').deleteOne({ _id: this.id });
+        return result;
     }
 }
 
