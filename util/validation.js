209ed674d0bd6a7c2e915a0b071e885ff05c7af2
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-01-17 13:57:01 -0500

    migrating the authentication to MVC

--- util/validation.js
+++ util/validation.js
@@ -1,10 +1,19 @@
 function postIsValid(title, content) {
-    return (title &&
-    content ||
-    title.trim() !== '' &&
-    content.trim() !== '' )
-}
-
-module.exports = {
-    postIsValid: postIsValid
-}
+    return title && content && title.trim() !== '' && content.trim() !== '';
+  }
+  
+  function userCredentialsAreValid(email, confirmEmail, password) {
+    return (
+      email &&
+      confirmEmail &&
+      password &&
+      password.trim().length >= 6 &&
+      email === confirmEmail &&
+      email.includes('@')
+    );
+  }
+  
+  module.exports = {
+    postIsValid: postIsValid,
+    userCredentialsAreValid: userCredentialsAreValid,
+  };
